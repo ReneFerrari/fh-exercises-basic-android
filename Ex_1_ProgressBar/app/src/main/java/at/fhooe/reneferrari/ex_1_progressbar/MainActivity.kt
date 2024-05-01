08b45dac6abc26c47ba2f4c01de0f7e29a30d2b1
ReneFerrari <rene_ferrari@outlook.de>
2024-05-01 21:58:35 +0200

    Add Exercise 1

---
+++ Ex_1_ProgressBar/app/src/main/java/at/fhooe/reneferrari/ex_1_progressbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package at.fhooe.reneferrari.ex_1_progressbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
